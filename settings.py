c415daa3726f09e4fd44d41e8bdbb0ff74954920
razluman <razluman@gmail.com>
2023-06-14 20:00:13 +0300

    modifié :         settings.py 	nouveau fichier : static/bootstrap.bundle.min.js 	nouveau fichier : static/bootstrap.min.css 	nouveau fichier : static/htmx.min.js 	nouveau fichier : "static/htmx.min.js\357\200\272Zone.Identifier" 	nouveau fichier : templates/base.html

--- settings.py
+++ settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = f"{env('PROJECT_NAME')}.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -146,6 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
