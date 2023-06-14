e8706f28ffc09681b92a79903f78f413cdbc2634
razluman <128676143+razluman@users.noreply.github.com>
2023-06-14 20:38:33 +0300

    Update settings_pgi.py

--- settings_pgi.py
+++ settings_pgi.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-$o9p7c+huq^cdbfd=m3dk+521l9niq$8l6(ubz(x@4b%*t0@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
